7671ac4898f1a3bfbfd2f76e7179ac43f4556f9d
Quentin DI MEO <QDM@localhost.localdomain>
2022-12-16 00:13:30 +0100

    [UPD] fine-tuning and gave clicking a use

--- semantic_web/pages/info.js
+++ semantic_web/pages/info.js
@@ -1,8 +1,5 @@
-import Head from "next/head";
-import Image from "next/image";
 import Router from "next/router";
 import { useState, useEffect } from "react";
-import styles from "../styles/Home.module.css";
 import fetch from "node-fetch";
 import axios from "axios";
 const WBK = require("wikibase-sdk");
